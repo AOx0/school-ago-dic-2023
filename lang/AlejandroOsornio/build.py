6a0536df34a5f63ddcdec644c8aa180c34cf5d3a
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-09-16 22:28:57 -0600

    Limpiar

--- lang/AlejandroOsornio/build.py
+++ lang/AlejandroOsornio/build.py
@@ -5,32 +5,31 @@ import sys
 def main():
     arg = sys.argv[1]
     if arg is None:
-       print("Falta indicar un número de ejercicio")
-       sys.exit(1)
+        print("Falta indicar un número de ejercicio")
+        sys.exit(1)
     ejercicio = None
     try:
-       ejercicio = int(arg)
+        ejercicio = int(arg)
     except Exception:
-       print("Ingrese un número de ejercicio válido 1..=6")
-       sys.exit(1)
+        print("Ingrese un número de ejercicio válido 1..=6")
+        sys.exit(1)
 
     # print(f"{ejercicio}")
 
     if not os.path.exists("./out/"):
-       os.makedirs("./out/")
+        os.makedirs("./out/")
 
     if ejercicio is not None and 1 <= ejercicio <= 6:
-       os.system(f"flex -L src/ejercicio{ejercicio}.lex")
-       os.system(f"gcc lex.yy.c -o ./out/ejercicio{ejercicio}")
-
+        os.system(f"flex -L src/ejercicio{ejercicio}.lex")
+        os.system(f"gcc lex.yy.c -o ./out/ejercicio{ejercicio}")
 
     if len(sys.argv) >= 4 and sys.argv[2] == "--":
-       print(f"Ejecutando ejercicio{ejercicio} con \
+        print(f"Ejecutando ejercicio{ejercicio} con \
 los argumentos \"{' '.join(sys.argv[3:])}\"")
-       os.system(f"./out/ejercicio{ejercicio} {' '.join(sys.argv[3:])}")
+        os.system(f"./out/ejercicio{ejercicio} {' '.join(sys.argv[3:])}")
     else:
-       print(f"Ejecutando ejercicio{ejercicio}")
-       os.system(f"./out/ejercicio{ejercicio}")
+        print(f"Ejecutando ejercicio{ejercicio}")
+        os.system(f"./out/ejercicio{ejercicio}")
 
 
 if __name__ == "__main__":
