f38c6644d921cc87087748d532bc28b506f0d55a
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-09-19 19:53:27 -0600

    Update

---
+++ lang/fuerza/src/main.rs
@@ -0,0 +1,15 @@
+struct HistElement<'a> {
+    symb: &'a str,
+    nt: bool,
+}
+
+fn main() {
+    let NT = ["E", "E'", "T", "T'", "F"];
+
+    let LHS = [(2, 1), (2, 3), (2, 5), (2, 7), (2, 9)];
+    let RHS = ["TE'", "+T", "+TE'", "F", "FT'", "*F", "*ET'", "(E)", "a"];
+
+    let hist: Vec<HistElement<'_>> = Vec::with_capacity(5 * 2);
+
+    println!("Hello, world!");
+}
