a5bc64c568d41203b7c56b584871ee9d7addf9f7
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-09-27 13:44:13 -0600

    Semaforo?

--- bd/semaforo/src/main.rs
+++ bd/semaforo/src/main.rs
@@ -1,27 +1,94 @@
-use axum::{extract::State, http::header, response::IntoResponse, routing::get, Router, Server};
+use axum::{
+    extract::State,
+    http::{header, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Router, Server,
+};
+use axum_extra::extract::{
+    cookie::{Cookie, Key},
+    PrivateCookieJar,
+};
 use std::sync::{Arc, Mutex};
 
-async fn root(State(lock): State<Arc<Mutex<bool>>>) -> impl IntoResponse {
-    let mut lock = lock.lock().unwrap();
+async fn root(State(state): State<AppState>, jar: PrivateCookieJar) -> impl IntoResponse {
+    let mut lock = state.lock.lock().unwrap();
 
-    if !(*lock) {
-        *lock = true;
+    if lock.is_none() {
+        let uuid = uuid::Uuid::new_v4().to_string();
+        let updated_jar = jar.add(Cookie::new("secret", uuid.clone()));
+        *lock = Some(uuid.clone());
         (
             [(header::CONTENT_TYPE, "text/plain")],
+            updated_jar,
             std::fs::read_to_string("./Cargo.toml").unwrap(),
         )
+    } else if let Some(secret) = jar.get("secret") {
+        if matches!(&*lock, Some(s) if secret.value() == s) {
+            (
+                [(header::CONTENT_TYPE, "text/plain")],
+                jar,
+                std::fs::read_to_string("./Cargo.toml").unwrap(),
+            )
+        } else {
+            let updated_jar = jar.remove(Cookie::named("secret"));
+            (
+                [(header::CONTENT_TYPE, "text/plain")],
+                updated_jar,
+                "Bloqueado".to_string(),
+            )
+        }
     } else {
         (
             [(header::CONTENT_TYPE, "text/plain")],
+            jar,
             "Bloqueado".to_string(),
         )
     }
 }
 
+async fn release(State(state): State<AppState>, jar: PrivateCookieJar) -> impl IntoResponse {
+    if let Some(secret) = jar.get("secret") {
+        let mut lock = state.lock.lock().unwrap();
+        if matches!(&*lock, Some(s) if secret.value() == s) {
+            let updated_jar = jar.remove(Cookie::named("secret"));
+            *lock = None;
+            return (
+                [(header::CONTENT_TYPE, "text/plain")],
+                updated_jar,
+                "Liberado".to_string(),
+            );
+        }
+    }
+    (
+        [(header::CONTENT_TYPE, "text/plain")],
+        jar,
+        "Sin permiso".to_string(),
+    )
+}
+
+#[derive(Clone)]
+struct AppState {
+    lock: Arc<Mutex<Option<String>>>,
+    key: Key,
+}
+
+impl axum::extract::FromRef<AppState> for Key {
+    fn from_ref(state: &AppState) -> Self {
+        state.key.clone()
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    let state = Arc::new(Mutex::new(false));
-    let router = Router::new().route("/", get(root)).with_state(state);
+    let state = AppState {
+        lock: Arc::new(Mutex::new(None)),
+        key: Key::generate(),
+    };
+    let router = Router::new()
+        .route("/", get(root))
+        .route("/release", get(release))
+        .with_state(state);
 
     Server::bind(&"[::]:8080".parse().unwrap())
         .serve(router.into_make_service())
