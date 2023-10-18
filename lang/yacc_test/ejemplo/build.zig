13dc75f96acd3b1de4fe6e0557a460188a93c5bd
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-10-18 10:55:27 -0600

    Working build.zig

--- lang/yacc_test/ejemplo/build.zig
+++ lang/yacc_test/ejemplo/build.zig
@@ -4,26 +4,28 @@ const os = std.os;
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const allocator = b.allocator;
+
+    const cwd = std.fs.cwd();
+
+    cwd.access("out", .{}) catch {
+        cwd.makeDir("out") catch {
+            const stddeb = std.io.getStdOut().writer();
+            stddeb.print("Failed to create {any}/out", .{cwd}) catch return;
+        };
+    };
 
     const argv = &[_][]const u8{ "/usr/bin/flex", "-L", "lexer.lex" };
-    const result = std.process.Child.exec(.{ .allocator = allocator, .argv = argv }) catch return;
-    defer {
-        allocator.free(result.stderr);
-        allocator.free(result.stdout);
-    }
     const argv2 = &[_][]const u8{ "/usr/bin/bison", "-l", "grammar.yacc" };
-    const result2 = std.process.Child.exec(.{ .allocator = allocator, .argv = argv2 }) catch return;
-    defer {
-        allocator.free(result2.stderr);
-        allocator.free(result2.stdout);
-    }
+
+    const flex = b.addSystemCommand(argv);
+    const bison = b.addSystemCommand(argv2);
 
     const exe = b.addExecutable(.{ .name = "yacc", .target = target, .optimize = optimize });
-
     b.installArtifact(exe);
     exe.linkLibC();
     exe.addCSourceFiles(&.{ "out/lexer.c", "out/parser.c" }, &.{ "-W", "-Wall", "-Wextra" });
+    exe.step.dependOn(&bison.step);
+    exe.step.dependOn(&flex.step);
 
     const run_cmd = b.addRunArtifact(exe);
 
@@ -35,13 +37,4 @@ pub fn build(b: *std.build.Builder) void {
 
     const run_step = b.step("run", "Ejecutar yacc");
     run_step.dependOn(&run_cmd.step);
-
-    switch (result.term) {
-        .Exited => |code| if (code != 0) return,
-        else => return,
-    }
-    switch (result2.term) {
-        .Exited => |code| if (code != 0) return,
-        else => return,
-    }
 }
